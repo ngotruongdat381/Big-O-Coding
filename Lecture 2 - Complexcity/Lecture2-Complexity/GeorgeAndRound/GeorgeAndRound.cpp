976a7b222df456c395168e5d9acb6952c5391bf7
Ngô Trường Đạt <ngotruongdat381@gmail.com>
2018-03-08 14:33:22 +0700

    update

---
+++ Lecture 2 - Complexcity/Lecture2-Complexity/GeorgeAndRound/GeorgeAndRound.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+
+#define maxn 111111
+int a[maxn], c[maxn];
+int cnt;
+
+int main()
+{
+	freopen("INPUT.INP", "rt", stdin);
+	int n, k;
+	cin >> n >> k;
+
+
+	return 0;
+}
