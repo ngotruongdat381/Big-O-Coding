a1018b260116ee3df105f8751db6805dceaff26b
Ngô Trường Đạt <ngotruongdat381@gmail.com>
2018-03-09 18:15:29 +0700

    Update

--- Lecture 2 - Complexcity/Lecture2-Complexity/GeorgeAndRound/GeorgeAndRound.cpp
+++ Lecture 2 - Complexcity/Lecture2-Complexity/GeorgeAndRound/GeorgeAndRound.cpp
@@ -4,16 +4,90 @@
 using namespace std;
 
 
-#define maxn 111111
-int a[maxn], c[maxn];
-int cnt;
+#define maxn 1111111
+int a[maxn], b[maxn], c[maxn];
+
 
 int main()
 {
+
 	freopen("INPUT.INP", "rt", stdin);
-	int n, k;
-	cin >> n >> k;
+	int n, m;
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> a[i];
+	}
+
+	for (int i = 1; i <= m; i++)
+	{
+		cin >> b[i];
+	}
 
 
+	//
+	for (int i = 1; i <= n; i++)
+	{
+		c[a[i]] = 1;
+	}
+
+	for (int i = 1; i <= m; i++)
+	{
+		if (c[b[i]] == 1)
+		{
+			c[b[i]] = 0;
+			b[i] = 0;
+		}
+	}
+
+	int ib = 0;
+	for (int ia = 1; ia <= n; ia++)
+	{
+		if (c[a[ia]] == 1)
+		{
+			while (ib <= m)
+			{
+				ib++;
+				if (b[ib] > a[ia])	//b[ib] != 0 && 
+				{
+					c[a[ia]] = 0;
+					break;
+				}
+			}
+		}
+	}
+
+	//int ia = 1, ib = 0;
+
+	//while (ia <= n && ib <= m)
+	//{
+	//	//ia++;
+	//	if (c[a[ia]] == 1)
+	//	{
+	//		ib++;
+
+	//		if (b[ib] != 0)
+	//		{
+	//			if (b[ib] > a[ia])
+	//				c[a[ia]] = 0;
+	//			else
+	//				continue;
+	//				//ib++;
+	//		}
+	//	}
+	//	ia++;
+	//}
+
+
+	int sum = 0;
+
+	for (int i = 1; i < maxn; i++)
+	{
+		sum += c[i];
+	}
+
+	cout << sum;
+	cin >> n;
 	return 0;
 }
