9c509ffa64b3fdd209f571faa2dd54f0d32342b9
Ngô Trường Đạt <ngotruongdat381@gmail.com>
2018-03-21 15:06:56 +0700

    Update BTTL 04

--- Lecture 4 - Stack&Queue/Lecture4-StackQueue/StreetParade/StreetParade.cpp
+++ Lecture 4 - Stack&Queue/Lecture4-StackQueue/StreetParade/StreetParade.cpp
@@ -5,9 +5,6 @@
 #include <queue>
 using namespace std;
 
-#define maxn 1111111
-int a[maxn];
-
 int main()
 {
 	freopen("INPUT.INP", "rt", stdin);
@@ -16,15 +13,9 @@ int main()
 	{
 		int n;
 		stack<int> sideStreet;
-		stack<int> street;
-		bool done = false;
 
-		cin >> n;
-
-		int rightOrder = 1;
-		int lastMobile;
+		cin >> n;		
 		
-
 		if (n == 0)
 		{
 			return 0;
@@ -35,55 +26,36 @@ int main()
 			cin >> a[i];
 		}
 
-		for (int i = 0; i < n; i++)
-		{
-			int mobile;
-			mobile = a[i];
+		int rightOrder = 1;
+		int i = 0;
 
-			if (mobile != rightOrder)
+		while(true)
+		{
+			if (i < n && a[i] == rightOrder)
 			{
-				if (sideStreet.empty() || mobile == sideStreet.top() - 1)
-				{
-					sideStreet.push(mobile);
-				}
-				else
-				{
-					cout << "no" << endl;
-					done = true;
-					break;
-				}
+				rightOrder++;
+				i++;
+
+			}
+			else if (!sideStreet.empty() && sideStreet.top() == rightOrder)
+			{
+				sideStreet.pop();
+				rightOrder++;
+
+			}
+			else if (i < n)
+			{
+				sideStreet.push(a[i]);
+				i++;
 			}
 			else
-			{
-				if (street.empty() || mobile == street.top() + 1)
-				{
-					street.push(mobile);
-					rightOrder++;
-
-					//extra
-					while (!sideStreet.empty() && street.top() + 1 == sideStreet.top())
-					{
-						street.push(sideStreet.top());
-						sideStreet.pop();
-						rightOrder++;
-					}
-					
-				}
-				else
-				{
-					cout << "no" << endl;
-					done = true;
-					break;
-				}
-			}
-
+				break;
 		}
 
-		if (!done && (sideStreet.empty() || street.top() < sideStreet.top()) )
-		{
+		if (rightOrder == n + 1)
 			cout << "yes" << endl;
-		}
-
+		else
+			cout << "no" << endl;
 	}
 
 	return 0;
