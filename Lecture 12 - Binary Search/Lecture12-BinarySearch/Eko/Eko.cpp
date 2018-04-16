9476a44282a306e49d96367e2a820aaf4afccf52
Ngô Trường Đạt <ngotruongdat381@gmail.com>
2018-04-16 23:48:05 +0700

    Update

---
+++ Lecture 12 - Binary Search/Lecture12-BinarySearch/Eko/Eko.cpp
@@ -0,0 +1,60 @@
+#include<iostream> 
+#include <list> 
+#include <stack> 
+#include <vector>
+#include <algorithm>
+#include <vector> 
+using namespace std;
+
+
+#define MAX 1000005
+
+int a[MAX];
+int dif[MAX];
+
+int main()
+{
+	freopen("INPUT.INP", "rt", stdin);
+
+	int N, M;
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++)
+	{
+		int num;
+		cin >> num;
+		a[i] = num;
+	}
+
+	sort(a, a + N);
+
+	dif[0] = a[N - 1] - a[N - 2];
+
+	for (int i = N - 1; i > 0; i--)
+	{
+		dif[N - i - 1] = dif[N - i - 2] + (a[i] - a[i - 1])*(N - i);
+	}
+
+	dif[N - 1] = dif[N - 2] + a[0] * N;
+
+	vector<int> v(dif, dif + N);
+
+	vector<int>::iterator lb;
+	lb = lower_bound(v.begin(), v.end(), M);
+	if (lb == v.end())
+		cout << "0";
+	else
+	{
+		int index = lb - v.begin();
+
+		int rest = *lb - M;
+
+		int tmp = rest / (index + 1);
+
+		int res = a[N - index - 2] + tmp;
+
+		cout << res;
+	}
+	
+	return 0;
+}
